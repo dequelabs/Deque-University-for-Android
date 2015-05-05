f7e3b9cf165fa3515a751591840ed74771afd4c0
Chris McMeeking <chris.mcmeeking@deque.com>
2015-05-05 15:33:19 -0400

    AAT-37 #time 3h #resolve Modified strings.  Added links for Android specific attributes.  Made textView links responsive.

--- app/src/main/java/com/dequesystems/accessibility101/introduction/AppIntroductionFragment.java
+++ app/src/main/java/com/dequesystems/accessibility101/introduction/AppIntroductionFragment.java
@@ -2,18 +2,33 @@ package com.dequesystems.accessibility101.introduction;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dequesystems.accessibility101.R;
 
 public class AppIntroductionFragment extends Fragment {
 
+    View mView;
+
+    TextView mTextView1;
+    TextView mTextView2;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_app_introduction, container, false);
+        mView = inflater.inflate(R.layout.fragment_app_introduction, container, false);
+
+        mTextView1 = (TextView) mView.findViewById(R.id.aacTextView1);
+        mTextView2 = (TextView) mView.findViewById(R.id.aacTextView2);
+
+        mTextView1.setMovementMethod(LinkMovementMethod.getInstance());
+        mTextView2.setMovementMethod(LinkMovementMethod.getInstance());
+
+        return mView;
     }
 }
--- app/src/main/java/com/dequesystems/accessibility101/labels/LabelsAboutFragment.java
+++ app/src/main/java/com/dequesystems/accessibility101/labels/LabelsAboutFragment.java
@@ -2,19 +2,44 @@ package com.dequesystems.accessibility101.labels;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dequesystems.accessibility101.R;
 
+import java.util.ArrayList;
+
 public class LabelsAboutFragment extends Fragment {
+
+    View mView;
+
+    ArrayList<TextView> mLinkedTextViews;
+
+    public LabelsAboutFragment() {
+        mLinkedTextViews = new ArrayList<>();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_labels_about, container, false);
+
+
+        mView = inflater.inflate(R.layout.fragment_labels_about, container, false);
+
+
+        mLinkedTextViews.add((TextView)mView.findViewById(R.id.aacTextView1));
+        mLinkedTextViews.add((TextView)mView.findViewById(R.id.aacTextView2));
+        mLinkedTextViews.add((TextView)mView.findViewById(R.id.aacTextView3));
+        mLinkedTextViews.add((TextView)mView.findViewById(R.id.aacTextView4));
+
+        for (TextView textView : mLinkedTextViews) {
+            textView.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
+        return mView;
     }
 
 }
