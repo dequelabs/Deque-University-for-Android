46b2c91d98778d1a00671eed176b6865355a96bf
Chris McMeeking <chris.mcmeeking@deque.com>
2015-05-27 15:42:40 -0400

    Deck screenshots are now stored in the DequeTestResults folder.

--- features/step_definitions/deque_steps.rb
+++ features/step_definitions/deque_steps.rb
@@ -1,24 +1,21 @@
 require 'net/http'
 require 'json'
 
+RESULTS_DIR = Dir.pwd + "/DequeTestResults"
+Dir.mkdir(RESULTS_DIR) unless File.exists?(RESULTS_DIR)
+
 Then(/^I perform DQTest (.*)$/) do |arg|
-	str = String.new(arg).downcase
 
-	result = ''
-
-	host = 'http://localhost:38383/a11ytest/' + str
-
-	uri = URI.parse(host)
-
+	uri = URI.parse('http://localhost:38383/a11ytest/' + String.new(arg).downcase)
 	http = Net::HTTP.new(uri.host, uri.port)
 	request = Net::HTTP::Get.new(uri.request_uri)
 
 	response = http.request(request)
 
-	sleep(1.0)
+	sleep(1.0) #TODO: This shouldn't be necessary.  Need to fix this in the A11yService
 
 	if (response.body.include?('FAIL'))
-		screenshot({:name=>"DequeTestResults/deque_test_failure.png"})
+		screenshot({:name=>RESULTS_DIR+"/deque_test_failure.png"})
 		puts "Deque Accessibility Test Failed"
 		json = JSON.parse(response.body)
 		puts response.body + "\n"
