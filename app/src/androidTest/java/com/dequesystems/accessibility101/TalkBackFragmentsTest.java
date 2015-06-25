a71522ea7be5ffdee4a2e9012f044abd1683ac8f
Melinda Kothbauer <melinda.kothbauer@deque.com>
2015-06-25 15:45:30 -0400

    DUA-17 #comment Updated unit test cases

--- app/src/androidTest/java/com/dequesystems/accessibility101/TalkBackFragmentsTest.java
+++ app/src/androidTest/java/com/dequesystems/accessibility101/TalkBackFragmentsTest.java
@@ -5,7 +5,8 @@ import android.test.UiThreadTest;
 import android.widget.TabHost;
 
 /**
- * Created by melindakothbauer on 6/25/15.
+ * Created by melinda.kothbauer@deque.com on 6/25/15.
+ * Tests the TalkBack story's fragments
  */
 public class TalkBackFragmentsTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
@@ -32,16 +33,16 @@ public class TalkBackFragmentsTest extends ActivityInstrumentationTestCase2<Main
 
     @UiThreadTest
     public void testTabs(){
-        mStoryManager.setActiveStory(6, mTabHost);
+        mStoryManager.setActiveStory(1, mTabHost);
 
         mTabHost.setCurrentTab(0);
-        assertNotNull(mStoryManager.getActiveStory().getTitle() + " about tab is null", mTabHost.getTabContentView());
-
+        assertNotNull(mStoryManager.getActiveStory().getTitle() + " about tab is null",  mTabHost.getTabContentView());
+        ;
         mTabHost.setCurrentTab(1);
-        assertNotNull(mStoryManager.getActiveStory().getTitle() + " broken tab is null", mTabHost.getTabContentView());
+        assertNotNull(mStoryManager.getActiveStory().getTitle() + " demos tab is null", mTabHost.getTabContentView());
 
         mTabHost.setCurrentTab(2);
-        assertNotNull(mStoryManager.getActiveStory().getTitle() + " fixed tab is null", mTabHost.getTabContentView());
+        assertNotNull(mStoryManager.getActiveStory().getTitle() + " advanced tab is null", mTabHost.getTabContentView());
     }
 
 }
