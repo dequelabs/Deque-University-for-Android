8d6b01619821def50fcde7d62d0ed176544d9dd4
Melinda Kothbauer <melinda.kothbauer@deque.com>
2015-10-16 11:16:23 -0400

    Labels & ContDesc Stories WCag info

--- app/src/main/java/com/dequesystems/accessibility101/contentdescriptions/ContDescBrokenFragment.java
+++ app/src/main/java/com/dequesystems/accessibility101/contentdescriptions/ContDescBrokenFragment.java
@@ -2,9 +2,11 @@ package com.dequesystems.accessibility101.contentdescriptions;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dequesystems.accessibility101.R;
 
@@ -12,11 +14,20 @@ public class ContDescBrokenFragment extends Fragment {
 
     View mImageViewCat;
 
+    private TextView mTextView1;
+    private TextView mTextView2;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_cont_desc_broken, container, false);
+
+        mTextView1 = (TextView) view.findViewById(R.id.aac_cont_desc_link_view_1);
+        mTextView2 = (TextView) view.findViewById(R.id.aac_cont_desc_link_view_2);
+
+        mTextView1.setMovementMethod(LinkMovementMethod.getInstance());
+        mTextView2.setMovementMethod(LinkMovementMethod.getInstance());
 
         mImageViewCat = view.findViewById(R.id.aac_contDescBroken_imageViewCat);
         /*
